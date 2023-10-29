a79e7a9cd74e0c1c7f029273634f2d5a05130ec6
sheninthjr <sheninthjr23@gmail.com>
2023-10-29 20:17:06 +0530

    Get Request

--- packages/prisma/server/index.ts
+++ packages/prisma/server/index.ts
@@ -15,6 +15,7 @@ const unit: { [key: string]: any } = {
   "5": prisma.unit5,
 };
 
+
 app.post("/post/:unit", async (req, res) => {
   const { parta1, parta2, partb1, partb2, partc1, partc2 } = req.body;
   const unitNo = req.params.unit;
@@ -30,6 +31,51 @@ app.post("/post/:unit", async (req, res) => {
   });
   res.json(unitQuestion);
 });
+
+app.get("/get/:testtype",async (req,res)=> {
+  const testType = req.params.testtype;
+  if (testType === "IA1") {
+    const partA11Questions = await prisma.unit1.findMany({
+      select: {
+        parta1: true,
+      },
+      take: 2,
+    });
+    const partA12Questions = await prisma.unit1.findMany({
+      select: {
+        parta2: true,
+      },
+      take: 1,
+    });
+    const partA21Questions = await prisma.unit2.findMany({
+      select: {
+        parta1: true,
+      },
+      take: 2,
+    });
+    const partB11Questions = await prisma.unit1.findMany({
+      select: {
+        partb1: true,
+        partb2: true
+      },
+      take: 1,
+    });
+    const partB21Questions = await prisma.unit2.findMany({
+      select: {
+        partb1: true
+      },
+      take: 1,
+    });
+    const partC1Questions = await prisma.unit1.findMany({
+      select: {
+        partc1: true
+      },
+      take: 1,
+    });
+    res.json({Questions:{partA11Questions,partA12Questions,partA21Questions,partB11Questions,partB21Questions,partC1Questions}});
+  }
+})
+
 
 const server = app.listen(3000, () => {
   console.log("Server is listenning on port 3000");
