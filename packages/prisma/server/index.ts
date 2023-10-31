07b2d058da14a5ed6a7bdc210a6ea2b98f1e02e6
sheninthjr <sheninthjr23@gmail.com>
2023-10-31 18:12:20 +0530

    Added Docker

--- packages/prisma/server/index.ts
+++ packages/prisma/server/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { PrismaClient, QType } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 const app = express();
