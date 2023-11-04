9b21cb92efa3fee00a5fd33cc4ce88ea5b3aeb43
sheninthjr <sheninthjr23@gmail.com>
2023-11-04 17:34:40 +0530

    Docker-compose

--- packages/prisma/deploy.sh
+++ packages/prisma/deploy.sh
@@ -1,7 +1,7 @@
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 cd /home/ubuntu/Automatic-Question-Generator
-git pull origin master
-yarn build
-pm2 stop express
-pm2 start npm --name "express" -- run "start:express"
+    git pull origin master
+    yarn build
+    pm2 stop express
+    pm2 start npm --name "express" -- run "start:express"
