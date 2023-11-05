1feb4bfa3e98a150df14941b2755d8ddf59e2e4d
sheninthjr <sheninthjr23@gmail.com>
2023-11-06 00:24:39 +0530

    Static in express

--- packages/prisma/server/index.ts
+++ packages/prisma/server/index.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import cors from "cors";
 import QuestionRouter  from './routes/QuestionRouter'
-
+import path from 'path';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/',QuestionRouter)
 
-
+app.use(express.static("public"));
+app.use("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+})
 
 const server = app.listen(3000, () => {
   console.log("Server is listenning on port 3000");
