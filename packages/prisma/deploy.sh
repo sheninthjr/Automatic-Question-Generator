1074b382f9bac8d84b71afe97ff237c15b3c519b
sheninthjr <sheninthjr23@gmail.com>
2023-11-07 02:14:59 +0530

    Deploy with npm

--- packages/prisma/deploy.sh
+++ packages/prisma/deploy.sh
@@ -2,4 +2,4 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 cd /home/ubuntu/Automatic-Question-Generator
     git pull origin master
-    npm --name "express" -- run "start:express"
+    npm run "start:express"
