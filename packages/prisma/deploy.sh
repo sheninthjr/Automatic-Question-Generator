ad42715f47771329533b227c0726d000c07fca90
sheninthjr <sheninthjr23@gmail.com>
2023-11-07 01:55:30 +0530

    express deploy.sh

--- packages/prisma/deploy.sh
+++ packages/prisma/deploy.sh
@@ -2,4 +2,6 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 cd /home/ubuntu/Automatic-Question-Generator
     git pull origin master
-    yarn start:express
+    yarn build
+    pm2 stop express
+    pm2 start npm --name "express" -- run "start:express"
